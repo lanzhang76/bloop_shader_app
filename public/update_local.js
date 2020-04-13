5a3ea87ebb9a26df540ab7e8b723c83affd3b608
Anna <annagarbier@gmail.com>
2020-04-13 14:43:03 -0400

    Shader responds to local textarea changes

---
+++ public/update_local.js
@@ -0,0 +1,10 @@
+// Reads the textarea, udpates the fragment code stored in frag_code,
+// and re-init()s the whole scene.
+
+const raw_fs = document.getElementById('code_text');
+const udpate_local_button = document.getElementById('update-local');
+
+udpate_local_button.addEventListener('click', function () {
+  frag_code = raw_fs.value;
+  init();
+});
