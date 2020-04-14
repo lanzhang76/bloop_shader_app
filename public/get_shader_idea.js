0c8ca6aae46104f5848dbd978cb4391f0ec0a032
Anna <annagarbier@gmail.com>
2020-04-13 20:01:24 -0400

    update helper link

--- public/get_shader_idea.js
+++ public/get_shader_idea.js
@@ -3,7 +3,7 @@
 const get_shader_idea_button = document.getElementById('getIdea');
 get_shader_idea_button.addEventListener('click', function () {
   window.open(
-    'https://github.com/lanzhang76/bloop_shader_app/helpers/examples.md',
+    'https://github.com/lanzhang76/bloop_shader_app/blob/master/helpers/examples.md',
     '_blank'
   );
 });
